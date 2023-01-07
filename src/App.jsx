b2a6830563e3d2f847d8197bde0456269773ea71
soubhagya-jitu <soubhagyasamal03@gmail.com>
2023-01-07 23:07:06 +0530

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React, { useState } from "react"
+import "./style.css"
 
 function App() {
+
     const [data, setData] = useState({
         date: ""
     })
+
+    let img = "https://www.nasa.gov/sites/all/themes/custom/nasatwo/images/nasa-logo.svg"
+    let day = "Discover the cosmos! Each day a different image or photograph of our fascinating universe is featured, along with a brief explanation written by a professional astronomer."
+    const [image, setImage] = useState(img)
+    const [information, setInformation] = useState(day)
 
     let name, value
     const handleInputs = (e) => {
@@ -28,17 +35,33 @@ function App() {
         if (!result.status) {
             window.alert(result.message)
         } else {
-            console.log(result.data)
+            setImage(result.data.url)
+            setInformation(result.data.explanation)
+
         }
+        // const fetchData = async () => {
+        //     const res = await fetch(
+        //       `https://api.nasa.gov/planetary/apod?api_key=8G0GNfFlRVtkuMhq3nfzH9bbxwhNLGjGDvmHXO2V&date=${date}`
+        //     );
+        //     const data = await res.json();
+        //     setImage(data.url);
+        //     setInformation(data.explanation)
+        // };
+
+        // fetchData();
     }
+
     return (
-            <div>
-                <div>
-                    <input type="text" name="date" id="date" autoComplete="off" value={data.date} onChange={handleInputs} placeholder="date"></input>
-                    <input type="submit" value="Get Image" onClick={postData}></input>
-                </div>
-                <img src="https://apod.nasa.gov/apod/image/2301/cg4_selby_960.jpg"></img>
+        <div className="main-card">
+            <h1>Astronomy Picture of the Day</h1>
+            <div className="card1">
+                <input type="text" name="date" id="date" autoComplete="off" value={data.date} onChange={handleInputs} placeholder="YYYY-MM-DD"></input>
+                <input type="submit" value="Get Image" id="submit" onClick={postData}></input>
             </div>
+            <img id = "main-image"src={image} alt="noimage"></img>
+            <h2>Explanation</h2>
+            <p>{information}</p>
+        </div>
 
     )
 }
